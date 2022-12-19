eeff3ec1fb295068b915dfb403c7a1a9e86a7488
Óscar <oscar.boullosa@estudiantat.upc.edu>
2022-12-19 13:12:58 +0100

    Final. Unity implementado con Android. FUNCIONA

--- app/src/main/java/edu/upc/eetac/dsa/unitytutorial3/MainActivity.java
+++ app/src/main/java/edu/upc/eetac/dsa/unitytutorial3/MainActivity.java
@@ -2,13 +2,24 @@ package edu.upc.eetac.dsa.unitytutorial3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+    }
+    public void onClick(View v){
+        Intent intent=new Intent(this, UnityPlayerActivity.class);
+        startActivity(intent);
     }
 }
